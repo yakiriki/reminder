c05db4a143f5c0b9dbd3948cbce8029131ad9383
yakiriki <dimsao@gmail.com>
2025-06-21 06:36:19 +0300

    Update errors.py

--- handlers/errors.py
+++ handlers/errors.py
@@ -3,6 +3,6 @@ from aiogram import Router, types
 router = Router()
 
 @router.errors()
-async def error_handler(update, exception):
+async def error_handler(event, exception):
     # Общий обработчик ошибок
     return True
