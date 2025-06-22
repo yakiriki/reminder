c432347dca5942a3f8d6de721eead362a2b71a9f
yakiriki <dimsao@gmail.com>
2025-06-22 20:41:20 +0300

    отчетыe db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import datetime
 from supabase import create_client, Client
 from config import Config
 
-supabase: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY, async_=True)
+supabase: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY)
 
 # USERS
 
